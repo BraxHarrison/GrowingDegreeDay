64a742a15506cc081f1c8c1e54b4c8b5d1719ea8
BraxHarrison <AngryAxolotl23>
2018-01-17 22:30:12 -0500

    POOSH

--- app/src/main/java/gdd/harrison/memdust/growingdegreeday/MapScreen.java
+++ app/src/main/java/gdd/harrison/memdust/growingdegreeday/MapScreen.java
@@ -37,6 +37,7 @@ public class MapScreen extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Context context;
+    private LocationInfo selectedLocation;
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
 
@@ -205,6 +206,9 @@ public class MapScreen extends FragmentActivity implements OnMapReadyCallback {
         }
         Marker customMarker = mMap.addMarker(new MarkerOptions().position(latLng).title(addresses.get(0).getLocality() + " " + latLng.toString()));
         customMarker.showInfoWindow();
+        selectedLocation = new LocationInfo(addresses.get(0).getLocality());
+        LatLng nearestLocLatLong = new LatLng(addresses.get(0).getLatitude(),addresses.get(0).getLongitude());
+        selectedLocation.setLatLong(nearestLocLatLong);
     }
 
 
