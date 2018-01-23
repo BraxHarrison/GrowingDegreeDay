9c552476ddbd5dd6c5180561b0f0241c252a82ca
dpgreenway <dpgreenway@bsu.edu>
2018-01-23 16:41:10 -0500

    Changed the launcher activity

--- app/src/main/java/gdd/harrison/memdust/growingdegreeday/MainHubActivity.java
+++ app/src/main/java/gdd/harrison/memdust/growingdegreeday/MainHubActivity.java
@@ -3,7 +3,7 @@ package gdd.harrison.memdust.growingdegreeday;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HubScreenExperimental extends AppCompatActivity {
+public class MainHubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
