67435da4d3ff2101d9377627d2a7c4f7741ea119
Danny Greenway <dpgreenway@bsu.edu>
2018-01-23 16:37:35 -0500

    Created a new hub activity with a blank template instead and created all the get location button and a Latitude and longitude.

---
+++ app/src/main/java/gdd/harrison/memdust/growingdegreeday/HubScreenExperimental.java
@@ -0,0 +1,13 @@
+package gdd.harrison.memdust.growingdegreeday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HubScreenExperimental extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub_screen_experimental);
+    }
+}
