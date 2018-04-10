7175a771c46e0e0ee902d09148954ffca212537a
BraxHarrison <AngryAxolotl23>
2018-04-10 12:04:58 -0400

    Table Screen access button added to hub screen

--- app/src/main/java/gdd/harrison/memdust/growingdegreeday/MainHubActivity.java
+++ app/src/main/java/gdd/harrison/memdust/growingdegreeday/MainHubActivity.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
 public class MainHubActivity extends AppCompatActivity {
 
     Button mapScreenSwitchButton;
+    Button tableScreenSwitchButton;
     Button graphScreenSwitchButton;
     Button dataFetchingButton;
     Button settingsButton;
@@ -69,6 +70,7 @@ public class MainHubActivity extends AppCompatActivity {
         mapScreenSwitchButton = findViewById(R.id.mapButton);
         graphScreenSwitchButton = findViewById(R.id.graphButton);
         dataFetchingButton = findViewById(R.id.getLocation);
+        tableScreenSwitchButton = findViewById(R.id.tableButton);
         latitude = findViewById(R.id.latitude);
         longitude = findViewById(R.id.longitude);
         settingsButton=findViewById(R.id.settingsButton);
@@ -78,7 +80,22 @@ public class MainHubActivity extends AppCompatActivity {
         listenForMapButtonClick(mapScreenSwitchButton);
         listenForGraphButtonClick(graphScreenSwitchButton);
         listenForDataFetchingClick(dataFetchingButton);
+        listenForTableButtonClick();
         listenForSettingsButtonClick();
+    }
+
+    private void listenForTableButtonClick() {
+        tableScreenSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToTableActivity();
+            }
+        });
+    }
+
+    private void switchToTableActivity() {
+        Intent tableViewIntent = new Intent(this, TableScreen.class);
+        startActivity(tableViewIntent);
     }
 
     protected void listenForSettingsButtonClick(){
