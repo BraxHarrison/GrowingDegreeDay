70fa847e8fe26235743d97c53636225e09f01c56
Danny Greenway <dpgreenway@bsu.edu>
2018-04-19 18:26:37 -0400

    Remove the brackets from the table view

--- app/src/main/java/gdd/harrison/memdust/growingdegreeday/TableScreen.java
+++ app/src/main/java/gdd/harrison/memdust/growingdegreeday/TableScreen.java
@@ -24,7 +24,6 @@ public class TableScreen extends AppCompatActivity{
         setContentView(R.layout.activity_table_screen);
         Intent thisIntent = getIntent();
         dataArray = thisIntent.getStringArrayExtra("dataForDisplay");
-        System.out.println(Arrays.toString(dataArray));
         smallSpinner = (Spinner) findViewById(R.id.spinner);
         largeSpinner = findViewById(R.id.spinner2);
         textView = findViewById(R.id.editText);
@@ -33,7 +32,9 @@ public class TableScreen extends AppCompatActivity{
     }
 
     void organizeLayersArray(){
-        String[] allLayers = dataArray[1].split(",");
+        String stringsWithRemovedFrontBrackets = dataArray[1].replace("[", "");
+        String stringWithRemovedBackBrackets = stringsWithRemovedFrontBrackets.replace("]", "");
+        String[] allLayers = stringWithRemovedBackBrackets.split(",");
         cornLayers = allLayers;
     }
 
