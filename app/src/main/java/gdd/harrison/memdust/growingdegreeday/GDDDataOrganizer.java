50fab0f292c1aa53662752ec9d839e14ddc42f69
Danny Greenway <dpgreenway@bsu.edu>
2018-04-18 21:10:38 -0400

    Refactor GDD Organizer and GDD Calculator. Still needs a little bit of work. The calculator still parses doubles which it shouldn't be responsible for!

--- app/src/main/java/gdd/harrison/memdust/growingdegreeday/GDDDataOrganizer.java
+++ app/src/main/java/gdd/harrison/memdust/growingdegreeday/GDDDataOrganizer.java
@@ -5,14 +5,13 @@ import java.util.Arrays;
 
 class GDDDataOrganizer {
 
-    protected String latitude;
-    protected String longitude;
+    private String latitude;
+    private String longitude;
     private String[] URLs = new String[4];
     private GDDDataCalculator calculator = new GDDDataCalculator();
     private String[] fetchedData;
     private double maturityValue = 72.0;
     private int currentDay;
-    private String[] cornGrowingStages = new String[5];
 
     private void buildURLs(){
         URLs[0] = "http://mrcc.isws.illinois.edu/U2U/gdd/controllers/datarequest.php?callgetCurrentData=1&lat=" + latitude +"&long=" +longitude;
@@ -48,7 +47,7 @@ class GDDDataOrganizer {
 
     String getAccumulatedAverage(){
         String[] accumulatedDataIntoArray = fetchedData[2].split(" ");
-        return removeExcessCharacters(String.valueOf(calculator.calculateTotalGDDAverage(accumulatedDataIntoArray)));
+        return removeExcessCharacters(String.valueOf(calculator.calculateTotalGDDAverage(organizeAccumulatedGDDs(accumulatedDataIntoArray))));
     }
 
 
@@ -56,7 +55,9 @@ class GDDDataOrganizer {
         String[] accumulatedModels = fetchedData[3].split(" ");
         String[] accumulatedDataIntoArray = fetchedData[2].split(" ");
         String[] accumulatedCurrentData = fetchedData[0].split(" ");
-        String gddProjection = removeExcessCharacters(String.valueOf(calculator.calculateGDDProjection(accumulatedDataIntoArray, accumulatedModels, accumulatedCurrentData)));
+        String gddProjection = removeExcessCharacters(String.valueOf(calculator.calculateGDDProjection(organizeAccumulatedGDDs(accumulatedDataIntoArray),
+                organizeModels(accumulatedModels, calculator.calculateDayNumber(calculator.getCurrentMonth(), calculator.getCurrentDayOfMonth())),
+                Double.parseDouble(accumulatedCurrentData[accumulatedCurrentData.length-1]))));
         currentDay = calculator.getModelDay();
         return gddProjection;
     }
@@ -64,6 +65,74 @@ class GDDDataOrganizer {
     int getCurrentDay(){
         return currentDay;
     }
+
+
+    private ArrayList<ArrayList<String>> organizeAccumulatedGDDs(String[] accumulatedData){
+        ArrayList<ArrayList<String>> listOfYears = new ArrayList<>();
+        int yearIndex = 0;
+        int currentYear = calculator.getCurrentYear();
+        if (calculator.isLeapYear(currentYear)){
+            for (int year = 1981; year < currentYear; year++){
+                int daysInYear = calculator.determineNumberOfDaysInYear(year);
+                listOfYears.add(getCorrectIndicesForLeapYear(accumulatedData,yearIndex,yearIndex+daysInYear));
+                yearIndex = yearIndex + daysInYear;
+            }
+        }
+        else{
+            for (int year = 1981; year < currentYear; year++){
+                int daysInYear = calculator.determineNumberOfDaysInYear(year);
+                listOfYears.add(getCorrectIndicesForNonLeapYear(accumulatedData,yearIndex, yearIndex+daysInYear));
+                yearIndex = yearIndex + daysInYear;
+            }
+        }
+        return listOfYears;
+    }
+
+    private ArrayList<ArrayList<String>> organizeModels(String[] modelData, int dayNumber){
+        ArrayList<ArrayList<String>> listOfModels = new ArrayList<>();
+        int modelBeginIndex = 0;
+        for (int i = 0; i < modelData.length/dayNumber; i++){
+            ArrayList<String> singleModel = new ArrayList<>();
+            singleModel.addAll(Arrays.asList(modelData).subList(modelBeginIndex, modelBeginIndex + dayNumber));
+            System.out.println(singleModel);
+            modelBeginIndex = modelBeginIndex + dayNumber;
+            listOfModels.add(singleModel);
+        }
+        return listOfModels;
+    }
+
+    private ArrayList<String> getCorrectIndicesForNonLeapYear(String[] accumulatedData, int yearBeginIndex, int yearEndIndex){
+        ArrayList<String> dataForSingleYear = new ArrayList<>();
+        int numberOfDaysInYear = yearEndIndex - yearBeginIndex;
+        for (int i = 0; i < numberOfDaysInYear; i++){
+            if ((numberOfDaysInYear == 366) && (i >= 59)){
+                dataForSingleYear.add(accumulatedData[i-1]);
+            }
+            else{
+                dataForSingleYear.add(accumulatedData[i]);
+            }
+
+        }
+        return dataForSingleYear;
+    }
+
+    private ArrayList<String> getCorrectIndicesForLeapYear(String[] accumulatedData, int yearBeginIndex, int yearEndIndex){
+        ArrayList<String> dataForSingleYear = new ArrayList<>();
+        int numberOfDaysInYear = yearEndIndex - yearBeginIndex;
+        for (int i = 0; i < numberOfDaysInYear + 1; i++){
+            if (i == 59){
+                dataForSingleYear.add(accumulatedData[i-1]);
+            }
+            else if(i > 59){
+                dataForSingleYear.add(accumulatedData[i-1]);
+            }
+            else{
+                dataForSingleYear.add(accumulatedData[i]);
+            }
+        }
+        return dataForSingleYear;
+    }
+
 
 
 
